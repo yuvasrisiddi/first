9e06121903df7eb653ab9442796d5dc2eb27e46c
yuva <yuva@gmail.com>
2025-06-06 07:10:23 +0530

    iam adding new feature-1

--- src/ex_01_Java_Basic/Lab001_hello.java
+++ src/ex_01_Java_Basic/Lab001_hello.java
@@ -1,9 +1,10 @@
 package ex_01_Java_Basic;
 
 public class Lab001_hello {
-    public static void main(String[] args){
-        System.out.println("hello,iam yuvasri");
+    public static void main(String[] args) {
+        System.out.println("hello,iam yuva");
 
     }
-
 }
+
+
