51a95e3e41d02aa5abbf63efb6e362955fe784eb
yuva <yuva@gmail.com>
2025-06-10 22:42:04 +0530

    iam adding new feature-3

--- src/ex_01_Java_Basic/varaibles.java
+++ src/ex_01_Java_Basic/varaibles.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basic;
 
 public class varaibles{
     public static void main(String[] args) {
-        int a = 9;
+        int a = 10;
         int b = 1;
         System.out.printf("%d * %d=%d\n", a, b, a * b);
         b++;
