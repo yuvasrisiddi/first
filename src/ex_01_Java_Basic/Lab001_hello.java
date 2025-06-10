ecdaa201c3d08aaa4841b8048e7549b01ac8d5c8
yuva <yuva@gmail.com>
2025-06-10 22:41:12 +0530

    iam adding new feature-1

--- src/ex_01_Java_Basic/Lab001_hello.java
+++ src/ex_01_Java_Basic/Lab001_hello.java
@@ -3,7 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_hello {
     public static void main(String[] args) {
         System.out.println("hello,iam yuva");
-
     }
 }
 
---
+++ src/ex_01_Java_Basic/varaibles.java
@@ -0,0 +1,29 @@
+package ex_01_Java_Basic;
+
+public class varaibles{
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+        System.out.printf("%d * %d=%d\n", a, b, a * b);
+        b++;
+
+    }
+}
