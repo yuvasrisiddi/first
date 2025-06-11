4a42a980ad93a63bd19c66c969ef428104e556b8
yuva <yuva@gmail.com>
2025-06-11 14:31:06 +0530

    iam adding new feature-3

---
+++ src/ex_01_Java_Basic/operators.java
@@ -0,0 +1,33 @@
+package ex_01_Java_Basic;
+
+public class operators {
+    public static void main(String[] args){
+//        int age=59;
+//        System.out.println(age);
+//        int a0=10;
+//        float b=32;
+//        System.out.println(a+b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+//        System.out.println("a+b");
+
+
+
+        int a=-10;
+        int a1=-12;
+        int result=a+a1;
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(result);
+
+        int b=-1;
+        b=b+1;
+        System.out.println(b);
+
+        int c=10;
+        int d=20;
+        System.out.println(d%c);
+
+    }
+}
---
+++ src/ex_01_Java_Basic/relational.java
@@ -0,0 +1,4 @@
+package ex_01_Java_Basic;
+
+public class relational {
+}
