c159811180a5f56ab9322f5cd6409086321d8e6e
yuva <yuva@gmail.com>
2025-06-30 23:37:03 +0530

    string

---
+++ src/test/String_Manipulation_Basics.java
@@ -0,0 +1,10 @@
+package test;
+
+public class String_Manipulation_Basics {
+    public static void main(String[] args){
+        String y="Hello world";
+        System.out.println(y.length());
+        System.out.println(y.charAt(0));
+        System.out.println(y.substring(5,11));
+    }
+}
